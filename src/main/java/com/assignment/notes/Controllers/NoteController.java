278522142c277b10ae66ae1d9ceb70d71d0a9116
Nayan1 <godhwaninayan12@gmail.com>
2024-01-04 19:00:19 +0530

    Created the SpringBoot Folder

---
+++ src/main/java/com/assignment/notes/Controllers/NoteController.java
@@ -0,0 +1,9 @@
+package com.assignment.notes.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoteController {
+
+}
